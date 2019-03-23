07a4c929c9e7dc5c6126e51f85d3e3469f098083
myGitUser13 <vanbeethoven01@yahoo.com>
2019-03-24 00:57:05 +0300

    Added withCredentials property

--- src/axios.js
+++ src/axios.js
@@ -4,6 +4,7 @@ export default axios.create({
  baseURL: 'http://localhost:1234',
  timeout: 1000,
  headers: {'X-Requested-With': 'XMLHttpRequest'},
+ withCredentials:true,
  transformResponse : [
   data => {
    console.log('Logging Data on Transform Response');
